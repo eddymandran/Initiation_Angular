e91f831d46cefa37690ad6ce953dc85b6bf2d423
eddymandran <eddy.mandran@gmail.com>
2021-11-08 14:19:47 +0100

    Preparation : nettoyage de l'appli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
