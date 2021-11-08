cf09f6273cef4d8ccd891c2335f9d84a3167292e
eddymandran <eddy.mandran@gmail.com>
2021-11-08 15:59:48 +0100

    Feature : creation du composant home + liste produits

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Person {name:string, age:number}
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  styleUrls: ['./about-us.component.css'],
+  // styles: ['mat-card {background:red}']
 })
-export class AboutUsComponent implements OnInit {
+export class AboutUsComponent {
+  persons : Person[] = [
+    {name:"eddy", age:32},
+    {name:"liam", age:3},
+    {name:"thea", age:1}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ]
 
 }
