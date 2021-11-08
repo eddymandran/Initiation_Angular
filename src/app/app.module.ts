c616b0deabc08da1d4d22901599737a56e702264
eddymandran <eddy.mandran@gmail.com>
2021-11-08 15:15:43 +0100

    creation composant about us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import {BrowserModule} from "@angular/platform-browser";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent
   ],
   imports: [
     CommonModule,
