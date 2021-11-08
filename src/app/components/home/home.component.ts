8341acf174b39de8ef3df0567e02c1ed8c255693
eddymandran <eddy.mandran@gmail.com>
2021-11-08 16:22:51 +0100

    import liste des produits dans composant home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {PRODUCTS} from "./mock-data";
 
+interface Product
+  {
+    id: number,
+    name: string,
+    price: number,
+    stock: number,
+    picture: string
+  }
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HomeComponent {
+products: Product[] = PRODUCTS;
 }
