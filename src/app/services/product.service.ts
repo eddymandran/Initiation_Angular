3bcc6d7bf5c795323bda847c66937be30c47f20b
eddymandran <eddy.mandran@gmail.com>
2021-11-10 10:31:34 +0100

    mise en place du .env pour l'Url + CSS card

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@angular/core';
 import { Observable} from 'rxjs';
 import { Product } from '../model/Product';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private API_URL = "http://localhost:5000/product";
   constructor(private http: HttpClient) {
 
   }
 
   getProducts():Observable<Product[]>{
-    return this.http.get<Product[]>(this.API_URL)
+    return this.http.get<Product[]>(environment.API.baseUrl + environment.API.endpoint.product)
   }
 
 }
