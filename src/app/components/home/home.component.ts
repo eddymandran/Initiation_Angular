160d31f04b61631a588119deff740a7902f1d786
eddymandran <eddy.mandran@gmail.com>
2021-11-10 11:19:05 +0100

    Feature : creation evenement sur bouton like uniquement dans composant home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Product} from '../../model/Product';
-import {Card} from '../../model/Card'; 
+import {Card} from '../../model/Card';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -29,4 +29,7 @@ export class HomeComponent implements OnInit{
 
   }
 
+  onLikeClick(){
+    console.log("Like clicked")
+  }
 }
