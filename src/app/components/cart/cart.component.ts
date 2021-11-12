f2e3e076c3e2bf154022da18370041ab4323dd3c
eddymandran <eddy.mandran@gmail.com>
2021-11-12 16:23:36 +0100

    implementation fonction delete en cours

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ShoppingCart} from "../../model/ShoppingCart";
+import {Product} from "../../model/Product";
+import {getLocaleCurrencyCode} from "@angular/common";
 
 @Component({
   selector: 'app-cart',
@@ -17,6 +19,17 @@ export class CartComponent implements OnInit {
     const localCart = localStorage.getItem('cart')
     this.cart = localCart!==null? JSON.parse(localCart):this.cart;
     this.cart.forEach(item => this.totalOrder+= (item.quantity * item.price))
+  }
+
+  onDeleteItem(newArticle:ShoppingCart){
+    let art = this.cart.find(article => article.id === newArticle.id);
+    if(art !== undefined && art.quantity>1){
+      art.quantity--
+    }else{
+      console.log("article a supprimer")
+      console.log(art)
+      console.log(this.cart)
+    }
   }
 
 }
